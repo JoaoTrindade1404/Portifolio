b05e6e7fd96530bf7494c36263df71739c563cce
João Trindade <saaspst2006@gmail.com>
2026-02-03 16:28:47 -0300

    feat: adding nav bar with animation

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export const Home = () => {
-  return (
-    <div className="overflow-hidden">Home</div>
-  ) 
-}
+  return <div className="overflow-hidden ">Home</div>;
+};
